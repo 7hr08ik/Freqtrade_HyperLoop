6369d9d2009bf1ec10bbf35a52ccc56fce7a0668
7hr08ik <7hr08ik@gmail.com>
2026-01-21 12:57:19 +0000

    Fixed config.py with sane defaults, not personal options

--- HyperLoop/config.py
+++ HyperLoop/config.py
@@ -9,16 +9,16 @@
 # ---------------------------
 
 # Number of hyperopt loops to complete
-NUM_RUNS = 10
+NUM_RUNS = 50
+
+# Hyperopt epochs per run
+EPOCHS = 400
+
+# Hyperopt threads to utilize
+THREADS = 14
 
 # Number of results to save
 TOP_N = 10
-
-# Hyperopt epochs per run
-EPOCHS = 5
-
-# Hyperopt threads to utilize
-THREADS = 10
 
 # Maximum number of HyperLoop log files to keep (0 = keep all)
 MAX_LOG_FILES = 10
@@ -28,10 +28,10 @@ MAX_LOG_FILES = 10
 # ---------------------------
 
 # Name of the strategy to test
-STRAT_NAME = "UberStrat"
+STRAT_NAME = "strat-name"
 
 # Name of the config file to use
-CONFIG_NAME = "config-hyp.json"
+CONFIG_NAME = "config.json"
 
 # Loss function to use
 HYPEROPT_LOSS = "SharpeHyperOptLoss"
@@ -42,7 +42,6 @@ HYPEROPT_LOSS = "SharpeHyperOptLoss"
 # MaxDrawDownHyperOptLoss
 # CalmarHyperOptLoss
 # ProfitDrawDownHyperOptLoss
-# MGM_WeightedMultiParameterHyperOptLoss
 
 # ---------------------------
 # Data settings
